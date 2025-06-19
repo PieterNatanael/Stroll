6b67d84daaf84231e022ef64bf7379afbca7922a
PieterNatanael <pieternatanael@icloud.com>
2025-06-19 08:41:18 +0800

    Initial Commit

---
+++ Stroll/StrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollApp.swift
+//  Stroll
+//
+//  Created by Pieter Yoshua Natanael on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StrollTests/StrollTests.swift
@@ -0,0 +1,17 @@
+//
+//  StrollTests.swift
+//  StrollTests
+//
+//  Created by Pieter Yoshua Natanael on 19/06/25.
+//
+
+import Testing
+@testable import Stroll
+
+struct StrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
