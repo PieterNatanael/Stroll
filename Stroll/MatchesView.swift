871d3b470354a7e5ee85b99dcc0bfc2773c5155b
PieterNatanael <pieternatanael@icloud.com>
2025-07-08 18:08:45 +0800

    Clean MatchesView

--- Stroll/MatchesView.swift
+++ Stroll/MatchesView.swift
@@ -146,7 +146,7 @@ struct HeaderView: View {
             Text("Make your move. They are waiting")
                 .font(.subheadline.bold())
                 .foregroundColor(.white.opacity(0.7))
-                .foregroundColor(.red)
+                .foregroundColor(.blue)
         }
         .padding(.horizontal)
     }
